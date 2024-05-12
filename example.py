899fa5ade3babe0b93146c4d3b90d596ce0fe92b
Lucas Yunkyu Lee <lucas@yklcs.com>
2024-05-12 17:22:52 +0900

    update example

--- example.py
+++ example.py
@@ -1,18 +1,17 @@
 import jaxsplat
 import jax
 import jax.numpy as jnp
-import numpy as np
 import imageio.v3 as iio
 import optax
 
 
 def main():
     key = jax.random.key(0)
-    iterations = 200
-    num_points = 1_000
+    iterations = 1000
+    num_points = 100_000
     gt_path = "test.png"
     out_path = "out.png"
-    lr = 0.05
+    lr = 0.01
 
     gt = jnp.array(iio.imread(gt_path)).astype(jnp.float32) / 255
 
@@ -24,32 +23,23 @@ def main():
 
     def loss_fn(params, coeffs, gt):
         output = render_fn(params, coeffs)
-        # loss = jnp.mean(optax.l2_loss(output, gt))
         loss = jnp.mean(jnp.square(output - gt))
         return loss
 
     with iio.imopen("out.mp4", "w", plugin="pyav") as video:
         video.init_video_stream("h264")
         for i in range(iterations):
-            img = (render_fn(params, coeffs) * 255).astype(jnp.uint8)
-            video.write_frame(img)
+            if i % 2 == 0:
+                img = (render_fn(params, coeffs) * 255).astype(jnp.uint8)
+                video.write_frame(img)
 
             loss, grads = jax.value_and_grad(loss_fn, argnums=0)(params, coeffs, gt)
             updates, optim_state = optim.update(grads, optim_state)
             params = optax.apply_updates(params, updates)
             print(f"{i=} {loss.item():.5f}")
 
-            # print("means3d", params["means3d"].min(), params["means3d"].max())
-            # print("scales", params["scales"].min(), params["scales"].max())
-            # print("quats", params["quats"].min(), params["quats"].max())
-            # print("colors", params["colors"].min(), params["colors"].max())
-            # print("opacities", params["opacities"].min(), params["opacities"].max())
-
-            # if loss < 1e-6:
-            #     break
-
-            # key, subkey = jax.random.split(key)
-            # params, coeffs = init(subkey, num_points, (gt.shape[1], gt.shape[0]))
+            if loss < 1e-3:
+                break
 
     out = render_fn(params, coeffs)
     iio.imwrite(out_path, (out * 255).astype(jnp.uint8))
@@ -60,8 +50,8 @@ def init(key, num_points, img_shape):
     means3d = jax.random.uniform(
         subkey,
         (num_points, 3),
-        minval=jnp.array([-2, -2, -1]),
-        maxval=jnp.array([2, 2, 1]),
+        minval=jnp.array([-6, -6, -1]),
+        maxval=jnp.array([6, 6, 1]),
         dtype=jnp.float32,
     )
 
@@ -72,8 +62,6 @@ def init(key, num_points, img_shape):
 
     key, subkey = jax.random.split(key)
     u, v, w = jax.random.uniform(subkey, (3, num_points, 1))
-    # quats = jax.ra  ndom.normal(subkey, (num_points, 4), dtype=jnp.float32)
-    # quats /= jnp.linalg.norm(quats, axis=-1, keepdims=True)
     quats = jnp.hstack(
         [
             jnp.sqrt(1 - u) * jnp.sin(2 * jnp.pi * v),
@@ -130,37 +118,25 @@ def init(key, num_points, img_shape):
 
 def render_fn(params, coeffs):
     means3d = params["means3d"]
-    quats = params["quats"] / (
-        jnp.linalg.norm(params["quats"], axis=-1, keepdims=True) + 1e-6
-    )
+    quats = params["quats"] / (jnp.linalg.norm(params["quats"], axis=-1, keepdims=True))
     scales = params["scales"]
     colors = jax.nn.sigmoid(params["colors"])
     opacities = jax.nn.sigmoid(params["opacities"])
 
-    (xys, depths, radii, conics, num_tiles_hit, cum_tiles_hit) = jaxsplat.project(
-        means3d,
-        scales,
-        quats,
-        coeffs["viewmat"],
+    img = jaxsplat.render(
+        means3d=means3d,
+        scales=scales,
+        quats=quats,
+        colors=colors,
+        opacities=opacities,
+        viewmat=coeffs["viewmat"],
+        background=coeffs["background"],
         img_shape=coeffs["img_shape"],
         f=coeffs["f"],
         c=coeffs["c"],
         glob_scale=coeffs["glob_scale"],
         clip_thresh=coeffs["clip_thresh"],
-        block_width=coeffs["block_size"],
-    )
-
-    img = jaxsplat.rasterize(
-        colors,
-        opacities,
-        coeffs["background"],
-        xys,
-        depths,
-        radii,
-        conics,
-        cum_tiles_hit,
-        img_shape=coeffs["img_shape"],
-        block_width=coeffs["block_size"],
+        block_size=coeffs["block_size"],
     )
 
     return img
