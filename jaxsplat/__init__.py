eb6470a4329aee3d7ac523a8b7c8c0bdf33efaa2
Lucas Yunkyu Lee <lucas@yklcs.com>
2024-05-15 19:37:57 +0900

    improve docstrings

--- jaxsplat/__init__.py
+++ jaxsplat/__init__.py
@@ -11,14 +11,36 @@ def render(
     opacities: jax.Array,
     *,
     viewmat: jax.Array,
-    background: jax.Array = jax.numpy.ones((1,)),
+    background: jax.Array,
     img_shape: tuple[int, int],
     f: tuple[float, float],
     c: tuple[int, int],
     glob_scale: float,
     clip_thresh: float,
     block_size: int = 16,
-):
+) -> jax.Array:
+    """
+    Renders 3D Gaussians to a 2D image differentiably.
+    Output is differentiable w.r.t. all non-keyword-only arguments.
+
+    Args:
+        means3d (Array): (N, 3) array of 3D Gaussian means
+        scales (Array): (N, 3) array of 3D Gaussian scales
+        quats (Array): (N, 4) array of 3D Gaussian quaternions, must be normalized
+        colors (Array): (N, 3) array of 3D Gaussian colors
+        opacities (Array): (N, 1) array of 3D Gaussian opacities
+    Keyword Args:
+        viewmat (Array): (4, 4) array containing view matrix
+        background (Array): (3,) array of background color
+        img_shape (tuple[int, int]): Image shape in (H, W)
+        f (tuple[float, float]): Focal lengths in (fx, fy)
+        c (tuple[int, int]): Principal points in (cx, cy)
+        glob_scale (float): Global scaling factor
+        clip_thresh (float): Minimum z depth clipping threshold
+        block_size (int): CUDA block size, 1 < block_size <= 16.
+    Returns:
+        Array: Rendered image
+    """
     (xys, depths, radii, conics, _num_tiles_hit, cum_tiles_hit) = project(
         means3d,
         scales,
